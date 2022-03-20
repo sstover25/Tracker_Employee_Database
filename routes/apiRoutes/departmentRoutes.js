6a21f60f0e0f2e5b92438bd1827e6139c0bbbc52
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-20 16:14:51 -0500

    created department POST route for adding new departments

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db/connections");
 
+// GET all departments
 router.get("/departments", (req, res) => {
   const sql = `SELECT * FROM department`;
 
@@ -13,6 +14,23 @@ router.get("/departments", (req, res) => {
     res.json({
       message: "success",
       data: rows,
+    });
+  });
+});
+
+// POST/Add a new department
+router.post("/department", ({ body }, res) => {
+  const sql = `INSERT INTO department (name)
+    VALUES (?)`;
+
+  db.query(sql, body.name, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
     });
   });
 });
