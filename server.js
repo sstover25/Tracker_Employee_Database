3c390035d7855cebf13c77b72ded8e134d470579
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-20 20:53:07 -0500

    added inquirer and added initial main menu question and options

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const inquirer = require("inquirer");
 const db = require("./db/connections");
 const apiRoutes = require("./routes/apiRoutes");
 
@@ -16,10 +17,36 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
+const promptMenuQuestions = () => {
+  console.log(`
+  ---------
+  Welcome to the Employee Tracker!
+  ---------
+  `);
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee's role",
+      ],
+      message: "Please select one of the following actions.",
+    },
+  ]);
+};
+
 db.connect((err) => {
   if (err) throw err;
   console.log("Database connected.");
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    // run inquirer
+    promptMenuQuestions();
   });
 });
