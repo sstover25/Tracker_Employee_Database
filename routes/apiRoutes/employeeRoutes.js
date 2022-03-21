da0fcfc1901b211ddee8c11fedb53c34b528b5fc
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-20 20:28:30 -0500

    tested and altered api calls and finalized them

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -9,9 +9,63 @@ router.get("/employees", (req, res) => {
   LEFT JOIN role ON employee.role_id = role.id
   LEFT JOIN department ON role.department_id = department.id
   LEFT OUTER JOIN employee AS employee2
-  ON employee.manager_id = employee2.id
+  ON employee.manager_id = employee2.id`;
 
-  `;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "succes",
+      data: rows,
+    });
+  });
+});
+
+// POST/Add a new employee
+router.post("/employee", ({ body }, res) => {
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+      VALUES (?,?,?,?)`;
+  const params = [
+    body.first_name,
+    body.last_name,
+    body.role_id,
+    body.manager_id,
+  ];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
+
+// PUT/Update an employee's role
+router.put("/employee/:id", (req, res) => {
+  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+  const params = [req.body.role_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Employee not found.",
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
 });
 
 module.exports = router;
